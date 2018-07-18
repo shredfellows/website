a57596514c0c305f4f2cd2d1c23253ca9fd5943a
Jennifer Bach <jmbach1@gmail.com>
2018-07-18 16:01:45 -0700

    updates

--- __tests__/src/App.test.js
+++ __tests__/src/App.test.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import ReactDOM from 'react-dom';
 
 import App from '../../../src/App.js';
 
 describe('<App /> (Enzyme Test)', () => {
 
-  it('it renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
-    ReactDOM.unmountComponentAtNode(div);
+  it('starts without crashing', () => {
+    let app = shallow(<App />);
+    expect(app.find('main').exists()).toBeTruthy();
   });
+
 });
 
 describe('<App /> (Snapshot Test)', () => {
