913a03967300c87244a6083c832acd170b7992f7
Justin Morris <justinjodymorris@gmail.com>
2018-07-17 17:07:21 -0700

    added styling to header and sidebar

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
-// import { faGithub } from '@fortawesome/free-solid-svg-icons';
 
+import ghlogo from '../../assets/img/github.png';
 import sflogo from '../../assets/img/shred-logo.png';
 
 import './header.css';
@@ -14,7 +14,7 @@ class Header extends Component {
       <header className="header">
           <FontAwesomeIcon icon={faBars}/>
           <img alt="shred fellows logo" src={sflogo}/>
-          {/* <FontAwesomeIcon icon={faGithub}/> */}
+          <img className="gh-logo" alt="github octocat logo" src={ghlogo}/>
       </header>
     );
   }
