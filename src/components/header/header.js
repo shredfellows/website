6dec1a5f618a3f38668781b62e5989ec2db5c5d3
Justin Morris <justinjodymorris@gmail.com>
2018-07-18 11:46:51 -0700

    added some intial login stuff

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,38 +1,92 @@
+import './header.css';
+
 import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import ghlogo from '../../assets/img/github.png';
 import sflogo from '../../assets/img/shred-logo.png';
-import './header.css';
+
+require('dotenv').config();
 
 class Header extends Component {
   constructor(props){
-    super(props){
+    super(props);
+
       this.state={loggedin: false}
 
       this.login=this.login.bind(this);
       this.signup=this.signup.bind(this);
     }
 
-    this.login(){
+
+    // formatLogin() {
+
+    //   const ENV = {};
+
+    //   ENV.isProduction = window.location.protocol === 'https:';
+    //   ENV.productionApiUrl = 'https://shred-fellows-server.herokuapp.com';
+    //   ENV.developmentApiUrl = 'http://localhost:3000';
+    //   ENV.apiURL = ENV.isProduction ? ENV.productionApiUrl : ENV.developmentApiUrl;
+      
+    //   let githubURL = "https://github.com/login/oauth/authorize";
+
+    //   let options = {
+    //     client_id: '252d0f262488210326f6',
+    //     scope: 'user,user:email',
+    //     redirect_uri: `${ENV.apiURL}/oauth`,
+    //   }
+
+    //   let QueryString = Object.keys(options).map((key, i) => {
+    //     return `${key}=` + encodeURIComponent(options[key]);
+    //   }).join("&");
+
+    //   let authURL = `${githubURL}?${QueryString}`;
+    //   ('#oauth').append(`<a href="${authURL}">Login With Google</a>`);
+    //   // res.redirect('https://github.com/login/oauth/authorize?client_id=252d0f262488210326f6&scope=user,user:email&redirect_uri=http://localhost:3000/oauth');
+    // }
+
+    login(){
 
     }
 
-    this.signup(){
+    signup(){
 
     }
-  }
+  
 
 
 
   render() {
+    const ENV = {};
+
+    ENV.isProduction = window.location.protocol === 'https:';
+    ENV.productionApiUrl = 'https://shred-fellows-server.herokuapp.com';
+    ENV.developmentApiUrl = 'http://localhost:3000';
+    ENV.apiURL = ENV.isProduction ? ENV.productionApiUrl : ENV.developmentApiUrl;
+
+    let githubURL = "https://github.com/login/oauth/authorize";
+
+    let options = {
+      client_id: '252d0f262488210326f6',
+      scope: 'user,user:email',
+      redirect_uri: `${ENV.apiURL}/oauth`,
+    }
+
+    let QueryString = Object.keys(options).map((key, i) => {
+      return `${key}=` + encodeURIComponent(options[key]);
+    }).join("&");
+
+    let authURL = `${githubURL}?${QueryString}`;
+      // res.redirect('https://github.com/login/oauth/authorize?client_id=252d0f262488210326f6&scope=user,user:email&redirect_uri=http://localhost:3000/oauth');
+    
     return (
       <header className="header">
           <FontAwesomeIcon icon={faBars}/>
           <img alt="shred fellows logo" src={sflogo}/>
-          <button onClick={this.signup}>Sign Up</button>
+        <a href={authURL}>LOGIN OR SIGNUP</a>
+          <img className="gh-logo" alt="github octocat logo" src={ghlogo} />
           <button onClick={this.login}>Log In</button>
-          <img className="gh-logo" alt="github octocat logo" src={ghlogo}/>
+         
       </header>
     );
   }
