5a9da6386419cf8c46a93d4e5dc977e83899d79c
Justin Morris <justinjodymorris@gmail.com>
2018-09-18 13:18:34 -0700

    added react proper eslint file and fixed following errors

--- src/store/reducers/users.js
+++ src/store/reducers/users.js
@@ -10,23 +10,23 @@ export default (state = initialState, action) => {
     case 'ADD_USER':
       return Object.assign({}, state, payload);
     
-    case 'ADD_ASSIGNMENT':
+    case 'ADD_ASSIGNMENT': {
       let assignments = state.assignments;
       assignments.push(payload);
       return Object.assign({}, state, {assignments});
-
-    case 'ADD_CODE_TO_USER':
+    }
+    case 'ADD_CODE_TO_USER': {
       let {assignmentName, code} = payload;
       let assignment = state.assignments.filter(single => single.assignmentName === assignmentName)[0];
       assignment.code = code;
       return Object.assign({}, state, assignment.code);
-    
-    case 'ADD_NOTE_TO_USER':
+    }
+    case 'ADD_NOTE_TO_USER': {
       let { assignName, notes } = payload;
       let singleAssignment = state.assignments.filter(single => single.assignmentName === assignName)[0];
       singleAssignment.notes = notes;
       return Object.assign({}, state, singleAssignment);
-      
+    }
     default:
       return state;
   }
