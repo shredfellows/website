5e01e30a5787902e197126737468a9b9892bd6fd
Justin Morris <justinjodymorris@gmail.com>
2018-09-17 19:16:07 -0700

    fixed common linting errors

--- src/lib/githubLogin.js
+++ src/lib/githubLogin.js
@@ -1,26 +1,27 @@
     
-    /**
-     * Enviroment variables required for github authorization.
-     */
-    const ENV = {};
+/**
+* Enviroment variables required for github authorization.
+*/
 
-    ENV.isProduction = !!window.location.host.match(/ccs/);
-    ENV.productionApiUrl = 'http://api.shredfellows.ccs.net';
-    ENV.developmentApiUrl = 'http://localhost:3000';
-    ENV.apiURL = ENV.isProduction ? ENV.productionApiUrl : ENV.developmentApiUrl;
+const ENV = {};
 
-    let githubURL = "https://github.com/login/oauth/authorize";
+ENV.isProduction = !!window.location.host.match(/ccs/);
+ENV.productionApiUrl = 'http://api.shredfellows.ccs.net';
+ENV.developmentApiUrl = 'http://localhost:3000';
+ENV.apiURL = ENV.isProduction ? ENV.productionApiUrl : ENV.developmentApiUrl;
+
+let githubURL = 'https://github.com/login/oauth/authorize';
 
 let clientId = ENV.isProduction ? '4476386c411872159ea9' : '0cdef2d78c2a55ed9343';
 
-    let options = {
-      client_id: clientId,
-      scope: 'user,user:email',
-      redirect_uri: `${ENV.apiURL}/oauth`,
-    }
+let options = {
+  client_id: clientId,
+  scope: 'user,user:email',
+  redirect_uri: `${ENV.apiURL}/oauth`,
+};
 
-    let QueryString = Object.keys(options).map((key, i) => {
-      return `${key}=` + encodeURIComponent(options[key]);
-    }).join("&");
+let QueryString = Object.keys(options).map((key) => {
+  return `${key}=` + encodeURIComponent(options[key]);
+}).join('&');
 
-    export const authURL = `${githubURL}?${QueryString}`;
+export const authURL = `${githubURL}?${QueryString}`;
