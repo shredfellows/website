f19daaf3dfc2f251c66f0ed85f2f0b65b3f4cae3
Justin Morris <justinjodymorris@gmail.com>
2018-09-25 16:57:25 -0700

    found font awesome GH logo, added the gh login button

--- src/components/landing/landing.js
+++ src/components/landing/landing.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 import './landing.css';
 
@@ -8,7 +10,14 @@ export default class Landing extends React.Component {
   render() {
     return(
       <section className="landing-overlay">
-        <img alt="shred fellows logo" src={sflogo} />
+        <div className="login-container">
+          <img alt="shred fellows logo" src={sflogo} />
+          <a href="#">
+            <FontAwesomeIcon icon={faGithub}/>
+            <p>Login with GitHub</p>
+          </a>
+          <p>- or - </p>
+        </div>
       </section>
     );
   }
