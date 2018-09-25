c01574723dd487de4e9644cd6fc12f3e929eba1e
Justin Morris <justinjodymorris@gmail.com>
2018-09-25 16:06:46 -0700

    added landing logo

--- src/components/landing/landing.js
+++ src/components/landing/landing.js
@@ -2,10 +2,14 @@ import React from 'react';
 
 import './landing.css';
 
+import sflogo from '../../assets/img/shred-logo-landing.png';
+
 export default class Landing extends React.Component {
   render() {
     return(
-      <section></section>
+      <section className="landing-overlay">
+        <img alt="shred fellows logo" src={sflogo} />
+      </section>
     );
   }
 }
