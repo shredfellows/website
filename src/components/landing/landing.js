9a6c19e2bae5e8a214e8c25c2a7dc04ae533dc86
Justin Morris <justinjodymorris@gmail.com>
2018-09-26 23:11:16 -0700

    added class name to form to keep styling specific to the landing page

--- src/components/landing/landing.js
+++ src/components/landing/landing.js
@@ -30,7 +30,7 @@ export default class Landing extends React.Component {
             <p>Login with GitHub</p>
           </a>
           <p>or</p>
-          <Form handler={this.handleSubmit}/>
+          <Form className="login-form" handler={this.handleSubmit}/>
         </div>
         <footer>
           <a href="" onClick={() => this.alertResponse('This Feature is underway.')}>Forgot Password?</a>
